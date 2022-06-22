a3c724a6155c8c1c3edecb731813ae7af352bde6
Miendjem Thierry <miendjemthierry01@gmail.com>
2022-06-22 10:51:08 +0100

    first commit

---
+++ database/migrations/2022_06_18_174619_create_order_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('pack_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->decimal('unitPrice');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
